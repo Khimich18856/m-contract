e63faa7f15e828a0270b75d11350f0b122a26313
Yuri Kosenko <krakoss@mail.ru>
2020-10-20 08:36:20 +0500

    class C

--- MContract/AppCode/C.cs
+++ MContract/AppCode/C.cs
@@ -12,7 +12,7 @@ namespace MContract.AppCode
 		{
 			get
 			{
-				return ConfigurationManager.AppSettings["production"] == "true" ? "http://mc.krakoss.ru/" : "http://localhost:3254";//http://m-contract.ru/
+				return ConfigurationManager.AppSettings["production"] == "true" ? "http://m-contract.ru/" : "http://localhost:3254";
 			}
 		}
 
@@ -20,7 +20,7 @@ namespace MContract.AppCode
 		{
 			get
 			{
-				return ConfigurationManager.AppSettings["production"] == "true" ? "http://mc.krakoss.ru/" : "http://localhost:3254/";//http://m-contract.ru/
+				return ConfigurationManager.AppSettings["production"] == "true" ? "http://m-contract.ru/" : "http://localhost:3254/";
 			}
 		}
 
